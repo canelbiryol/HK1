5d3b10e5dbc77f7a917018553c769bc253210984
mangkj <michaelakj@hotmail.com>
2018-04-14 11:59:26 -0400

    Add files via upload

---
+++ MatrixTools/PreparePredictors.py
@@ -0,0 +1,62 @@
+'''
+Created on Apr 14, 2018
+
+@author: Michael
+'''
+import numpy as np
+import math
+
+class PreparePredictors(object):
+    '''
+    Code that returns the g vector.
+    Prepares the min variance predictor, omniscient predictor and random long-short predictor
+    '''
+    
+    def __init__(self, outOfSampleStockReturns):
+        '''
+        Load the out-of-sample stock returns.
+        outOfSampleStockReturns[i] is an array containing the returns of the ith stock.
+        '''
+        self.N = len(outOfSampleStockReturns)
+        self.NormalizationFactor = math.sqrt(self.N)
+        
+        N_ticks = len(outOfSampleStockReturns[0])
+        for k in outOfSampleStockReturns:
+            if len(k)!=N_ticks:
+                raise Exception('Each stock much have the same amount of input data.')
+        
+        
+        '''For each stock compute the net return over the period'''
+        self.netReturns = np.zeros(self.N)
+        for x in range(self.N):
+            netReturn = 1
+            for y in range(N_ticks):
+                netReturn *= 1+outOfSampleStockReturns[x][y]
+            self.netReturns[x] = netReturn - 1
+        
+    
+    def getMinVarPredictor(self):
+        return self.NormalizationFactor*np.ones(self.N)
+        
+    def getOmniscientPredictor(self):
+        return self.NormalizationFactor*self.netReturns
+    
+    def getRandomPredictor(self):
+        #Produce a random vector on the unit sphere
+        random = np.random.rand(self.N)
+        magnitude = 0
+        for x in random:
+            magnitude += x*x
+        random /= math.sqrt(magnitude)
+        
+        return self.NormalizationFactor*random
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
