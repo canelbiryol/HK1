0f4ea9443b62a675ad6133b7517c03f9a3435470
mangkj <michaelakj@hotmail.com>
2018-04-15 01:56:08 -0400

    Add files via upload

---
+++ MatrixTools/CovarianceMatrices.py
@@ -0,0 +1,40 @@
+'''
+Created on Apr 14, 2018
+
+@author: Michael
+'''
+import numpy as np
+import pyRMT
+from sklearn.covariance import ledoit_wolf
+'''Class that produces the covariance matrices.
+    Performs all the usual checks.
+'''
+
+class CovarianceCalculator(object):
+    
+    '''stockReturns[i] is an array containing the returns of the ith stock.
+    '''
+    def __init__(self, stockReturns):
+        N_ticks = len(stockReturns[0])
+        for k in stockReturns:
+            if len(k)!=N_ticks:
+                raise Exception('Each stock much have the same amount of input data.')
+        
+        self.stockReturns = stockReturns
+        self.N_stocks = len(self.stockReturns)
+        self.N_ticks = len(self.stockReturns[0])
+        
+    def getEmpiricalCovariance(self):
+        return np.cov(self.stockReturns)
+    
+    def getClippedCovariance(self):
+        return pyRMT.clipped(np.transpose(self.stockReturns), return_covariance=True)
+    
+    def getLedoitWolfCovariance(self):
+        return ledoit_wolf(np.transpose(self.stockReturns))[0]
+    
+    
+
+
+if __name__ == '__main__':
+    pass
---
+++ MatrixTools/Test_CovarianceMatrices.py
@@ -0,0 +1,38 @@
+'''
+Created on Apr 15, 2018
+
+@author: Michael
+'''
+import unittest
+import numpy as np
+from MatrixTools.CovarianceMatrices import CovarianceCalculator
+
+
+class Test(unittest.TestCase):
+
+
+    def testCovarianceMatrices(self):
+        '''First we generate some fake data.'''
+        L = np.array([[2, 0, 0],[1, 3, 0],[0.5, 0.4, 1]])
+        print('True covariance is',np.dot(L,np.transpose(L)))
+        
+        Y = np.zeros((3, 10000))
+        for i in range(10000):
+            x1 = np.random.normal()
+            x2 = np.random.normal()
+            x3 = np.random.normal()
+            
+            Y[0,i] = 2*x1
+            Y[1,i] = 1*x1+3*x2
+            Y[2,i] = 0.5*x1+0.4*x2+1*x3
+        
+        C = CovarianceCalculator(Y)
+        print('Empirical covariance is',C.getEmpiricalCovariance())
+        print('Clipped covariance is',C.getClippedCovariance())
+        print('LW covariance is',C.getLedoitWolfCovariance())
+        
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testCovarianceMatrices']
+    unittest.main()
